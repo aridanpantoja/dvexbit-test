1ce76fbf05d3ef5d63c6bb6f8017b281e91200f5
Aridan Pantoja <aridanpantoja@gmail.com>
2025-03-10 00:12:14 -0300

    refactor: add useTable hook for table functionality

---
+++ src/hooks/use-table.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  RowSelectionState,
+  SortingState,
+  useReactTable,
+} from '@tanstack/react-table'
+import { useState } from 'react'
+
+export function useTable<TData, TValue>(
+  data: TData[],
+  columns: ColumnDef<TData, TValue>[],
+) {
+  const [rowSelection, setRowSelection] = useState<RowSelectionState>({})
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+  const [sorting, setSorting] = useState<SortingState>([])
+
+  const table = useReactTable({
+    data,
+    columns,
+    enableRowSelection: true,
+    onRowSelectionChange: setRowSelection,
+    onColumnFiltersChange: setColumnFilters,
+    onSortingChange: setSorting,
+    getFilteredRowModel: getFilteredRowModel(),
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    state: {
+      sorting,
+      columnFilters,
+      rowSelection,
+    },
+  })
+
+  return { table, rowSelection, columnFilters, sorting }
+}
