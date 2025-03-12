45ca549c82e585274af8d5869f92cc9a8b3b256d
Aridan <aridanpantoja@gmail.com>
2025-03-12 17:26:09 -0300

    feat: add task page, not found page, delete row actionsand add axios and concurrently

--- src/app/tasks/[taskId]/page.tsx
+++ src/app/tasks/[taskId]/page.tsx
@@ -1,3 +1,60 @@
+import { TaskForm } from '@/components/task-form'
+import { Button } from '@/components/ui/button'
+import { WidthWrapper } from '@/components/width-wrapper'
+import { ChevronLeft, CircleDashed, Pencil, Plus, Trash2 } from 'lucide-react'
+import Link from 'next/link'
+
 export default function TaskPage() {
-  return <></>
+  return (
+    <article className="my-24 space-y-8">
+      <section>
+        <WidthWrapper>
+          <div className="flex items-center justify-between">
+            <Link
+              href="/"
+              className="flex w-fit items-center gap-1 font-semibold"
+            >
+              <ChevronLeft /> Back to home
+            </Link>
+
+            <div className="space-x-2">
+              <Button size="icon" variant="outline">
+                <Pencil />
+              </Button>
+
+              <Button size="icon" variant="outline">
+                <Trash2 />
+              </Button>
+            </div>
+          </div>
+        </WidthWrapper>
+      </section>
+
+      <section>
+        <WidthWrapper>
+          <div className="flex flex-col gap-5">
+            <h1>DvexFlow - Manage tasks efficiently.</h1>
+            <p className="text-muted-foreground text-lg">
+              DvexFlow is a modern and intuitive task management platform
+              designed to optimize productivity and organization for teams and
+              professionals. With features such as task creation, editing, and
+              filtering, the tool enables efficient workflow tracking.
+            </p>
+
+            <div className="space-x-2">
+              <Button>
+                <CircleDashed /> Todo
+              </Button>
+              <Button>
+                <CircleDashed /> Todo
+              </Button>
+              <Button>
+                <CircleDashed /> Todo
+              </Button>
+            </div>
+          </div>
+        </WidthWrapper>
+      </section>
+    </article>
+  )
 }
--- src/components/data-table/columns.tsx
+++ src/components/data-table/columns.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import { ColumnHeader } from '@/components/data-table/column-header'
-import { RowActions } from '@/components/data-table/row-actions'
+import { statuses } from '@/data/data'
 import { Task } from '@/data/schema'
-import { capitalize, getDaysRemaining } from '@/lib/utils'
+import { getDaysRemaining } from '@/lib/utils'
 import { ColumnDef } from '@tanstack/react-table'
 import { ALargeSmall, BookOpen, CalendarDays, CircleDashed } from 'lucide-react'
 import Link from 'next/link'
@@ -40,12 +40,30 @@ export const columns: ColumnDef<Task>[] = [
     header: ({ column }) => (
       <ColumnHeader column={column} title="Status" icon={CircleDashed} />
     ),
-    cell: ({ row }) => (
-      <div className="flex w-fit items-center gap-1.5 rounded-full bg-red-300 px-2.5 py-0.5">
-        <div className="size-2.5 rounded-full bg-red-500"></div>
-        {capitalize(row.getValue('status'))}
-      </div>
-    ),
+    cell: ({ row }) => {
+      const status = statuses.find(
+        (status) => status.value === row.getValue('status'),
+      )
+
+      if (!status) {
+        return null
+      }
+
+      return (
+        <div className="flex items-center gap-2">
+          {status.icon && (
+            <status.icon className="text-muted-foreground size-4" />
+          )}
+          <span>{status.label}</span>
+        </div>
+      )
+    },
+    // cell: ({ row }) => (
+    //   <div className="flex w-fit items-center gap-1.5 rounded-full bg-red-300 px-2.5 py-0.5">
+    //     <div className="size-2.5 rounded-full bg-red-500"></div>
+    //     {capitalize(row.getValue('status'))}
+    //   </div>
+    // ),
     filterFn: (row, id, value) => {
       return value.includes(row.getValue(id))
     },
@@ -77,9 +95,5 @@ export const columns: ColumnDef<Task>[] = [
         )
       }
     },
-  },
-  {
-    id: 'actions',
-    cell: ({ row }) => <RowActions row={row} />,
   },
 ]
